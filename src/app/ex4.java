c3761cd9220802562967b9177cc81d696568aa6a
kkm0406 <50093283+kkm0406@users.noreply.github.com>
2021-10-15 17:52:19 +0900

    Delete src/app directory

--- src/app/ex4.java
+++
@@ -1,9 +0,0 @@
-package app;
-import base.Shape;
-import derived.Circle;
-public class ex4 {
-    public static void main(String[] args) {
-        Shape shape = new Circle();
-        shape.draw();
-    }
-}
