0f8edfd27c199587818a2de8bf50d4b705e4929a
kkm0406 <50093283+kkm0406@users.noreply.github.com>
2021-10-22 22:26:07 +0900

    Update ex7_4.java

--- src7/example/ex7_4.java
+++ src7/example/ex7_4.java
@@ -4,18 +4,17 @@ import java.util.Vector;
 public class ex7_4 {
     public static void main(String[] args) {
         Vector<Integer> v = new Vector<Integer>();
-        v.add(5); // 5 삽입
-        v.add(4); // 4 삽입
-        v.add(-1); // -1 삽입
-        v.add(2, 100); // 4와 -1 사이에 정수 100 삽입
-// Iterator를 이용한 모든 정수 출력하기
+        v.add(5);
+        v.add(4);
+        v.add(-1);
+        v.add(2, 100); 
         Iterator<Integer> it = v.iterator();
         while(it.hasNext()) {
             int n = it.next();
             System.out.println(n);
         }
         int sum = 0;
-        it = v.iterator(); // Iterator 객체 얻기
+        it = v.iterator(); 
         while(it.hasNext()) {
             int n = it.next();
             sum += n;
