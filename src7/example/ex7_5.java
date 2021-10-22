4ea3d8ef84145e9ee2340674443110ad46150bba
kkm0406 <50093283+kkm0406@users.noreply.github.com>
2021-10-22 22:26:33 +0900

    Update ex7_5.java

--- src7/example/ex7_5.java
+++ src7/example/ex7_5.java
@@ -5,11 +5,10 @@ public class ex7_5 {
     public static void main(String[] args) {
         HashMap<String, String> dic =
                 new HashMap<String, String>();
-// 3 개의 (key, value) 쌍을 dic에 저장
-        dic.put("baby", "아기"); // "baby"는 key, "아기"은 value
+        dic.put("baby", "아기");
         dic.put("love", "사랑");
         dic.put("apple", "사과");
-// 영어 단어를 입력받고 한글 단어 검색. "exit" 입력받으면 종료
+
         Scanner scanner = new Scanner(System.in);
         while(true) {
             System.out.print("찾고 싶은 단어는?");
