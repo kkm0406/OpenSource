0aff29ec74a3987852744e8afe1bbb0f63a5efc2
kkm0406 <50093283+kkm0406@users.noreply.github.com>
2021-10-22 22:27:02 +0900

    Update ex7_6.java

--- src7/example/ex7_6.java
+++ src7/example/ex7_6.java
@@ -4,10 +4,9 @@ import java.util.Set;
 
 public class ex7_6 {
     public static void main(String[] args) {
-        // 사용자 이름과 점수를 기록하는 HashMap 컬렉션 생성
         HashMap<String, Integer> javaScore =
                 new HashMap<String, Integer>();
-// 5 개의 점수 저장
+
         javaScore.put("김성동", 97);
         javaScore.put("황기태", 88);
         javaScore.put("김남윤", 98);
@@ -15,11 +14,8 @@ public class ex7_6 {
         javaScore.put("한원선", 99);
         System.out.println("HashMap의 요소 개수 :"
                 + javaScore.size());
-// 모든 사람의 점수 출력.
-// javaScore에 들어 있는 모든 (key, value) 쌍 출력
-// key 문자열을 가진 집합 Set 컬렉션 리턴
+
         Set<String> keys = javaScore.keySet();
-// key 문자열을 순서대로 접근할 수 있는 Iterator 리턴
         Iterator<String> it = keys.iterator();
         while(it.hasNext()) {
             String name = it.next();
