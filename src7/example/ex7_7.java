3bc1858b9807a83970b98c7b40e6a844ac3f2e5f
kkm0406 <50093283+kkm0406@users.noreply.github.com>
2021-10-22 22:27:29 +0900

    Update ex7_7.java

--- src7/example/ex7_7.java
+++ src7/example/ex7_7.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 import java.util.Scanner;
 
-class Student { // 학생을 표현하는 클래스
+class Student { 
     int id;
     String tel;
     public Student(int id, String tel) {
@@ -19,17 +19,17 @@ class Student { // 학생을 표현하는 클래스
 public class ex7_7 {
     public static void main(String[] args) {
         HashMap<String, Student> map = new HashMap<String, Student>();
-// 3 명의 학생 저장
+
         map.put("황기태", new Student(1, "010-111-1111"));
         map.put("이재문", new Student(2, "010-222-2222"));
         map.put("김남윤", new Student(3, "010-333-3333"));
         Scanner scanner = new Scanner(System.in);
         while(true) {
             System.out.print("검색할 이름?");
-            String name = scanner.nextLine(); // 사용자로부터 이름 입력
+            String name = scanner.nextLine(); 
             if(name.equals("exit"))
-                break; // while 문을 벗어나 프로그램 종료
-            Student student = map.get(name); // 이름에 해당하는 Student 객체 검색
+                break; 
+            Student student = map.get(name); 
             if(student == null)
                 System.out.println(name + "은 없는 사람입니다.");
             else
